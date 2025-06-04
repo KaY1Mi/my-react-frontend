6710812d0d159fcd5e1b7593bf271421328aa2c9
KaY1Mi <mkrivenko28@gmail.com>
2025-06-04 18:59:24 +1000

    обавил upabase torage для загрузки аватарок

--- src/Components/Pages/supabaseClient.js
+++ src/Components/Pages/supabaseClient.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dogesupyybzlvkbwyaak.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvZ2VzdXB5eWJ6bHZrYnd5YWFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwMDY3NjYsImV4cCI6MjA2NDU4Mjc2Nn0.RkTGbJcwZGtMncnLl82UltLSjRTc7EOA7VpbMds0W5U';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    detectSessionInUrl: true,
+    storage: localStorage, // <== важно!
+  },
+});
