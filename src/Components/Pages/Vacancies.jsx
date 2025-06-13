86d8d9d2a1f7df2e57e2e2c3e0e902edf2783403
KaY1Mi <mkrivenko28@gmail.com>
2025-06-13 18:29:42 +1000

    vacancie

--- src/Components/Pages/Vacancies.jsx
+++ src/Components/Pages/Vacancies.jsx
@@ -12,8 +12,8 @@ const Vacancies = () => {
   const sectionsRef = useRef([]);
   const scrollTimeout = useRef(null);
   const navigate = useNavigate();
-  const { language } = useContext(LanguageContext); // Get language first
-  const t = translations[language]; // Then use it for translations
+  const { language } = useContext(LanguageContext); 
+  const t = translations[language]; 
 
   useEffect(() => {
       const checkIfMobile = () => {
@@ -119,7 +119,7 @@ return (
             <p className='font-manrope font-medium text-[13px] uppercase self-center text-right md:col-span-2  md:text-base'>{t.p_vac_name_two}</p>
             <h4 className='font-bebas text-2xl col-span-full'>{t.h4_price_one_two}</h4>
             <h5 className='font-manrope text-sm col-span-full md:text-base'>{t.h5_text_one_two}</h5>
-            <button onClick={()=>navigate('/vacancie-one')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
+            <button onClick={()=>navigate('/vacancie-two')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
           </div>
         </div>
 
@@ -130,7 +130,7 @@ return (
             <p className='font-manrope font-medium text-[13px] uppercase self-center text-right md:col-span-2 md:text-base'>{t.p_vac_name_three}</p>
             <h4 className='font-bebas text-2xl col-span-full'>{t.h4_price_one_three}</h4>
             <h5 className='font-manrope text-sm col-span-full md:text-base'>{t.h5_text_one_three}</h5>
-            <button onClick={()=>navigate('/vacancie-one')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
+            <button onClick={()=>navigate('/vacancie-three')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
           </div>
         </div> 
 
@@ -145,7 +145,7 @@ return (
             <p className='font-manrope font-medium text-[13px] uppercase self-center text-right md:col-span-2  md:text-base'>{t.p_vac_name_two}</p>
             <h4 className='font-bebas text-2xl col-span-full'>{t.h4_price_one_two}</h4>
             <h5 className='font-manrope text-sm col-span-full md:text-base'>{t.h5_text_one_two}</h5>
-            <button onClick={()=>navigate('/vacancie-one')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
+            <button onClick={()=>navigate('/vacancie-four')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
           </div>
         </div>
 
@@ -177,7 +177,7 @@ return (
           <p className='font-manrope font-medium text-[13px] uppercase self-center text-right md:col-span-2 md:text-base'>{t.p_vac_name_six}</p>
           <h4 className='font-bebas text-2xl col-span-full'>{t.h4_price_one_six}</h4>
           <h5 className='font-manrope text-sm col-span-full md:text-base'>{t.h5_text_one_six}</h5>
-          <button onClick={()=>navigate('/vacancie-five')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
+          <button onClick={()=>navigate('/vacancie-six')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
         </div>
       </div>
 
@@ -188,7 +188,7 @@ return (
           <p className='font-manrope font-medium text-[13px] uppercase self-center text-right md:col-span-2 md:text-base'>{t.p_vac_name_seven}</p>
           <h4 className='font-bebas text-2xl col-span-full'>{t.h4_price_one_seven}</h4>
           <h5 className='font-manrope text-sm col-span-full md:text-base'>{t.h5_text_one_seven}</h5>
-          <button onClick={()=>navigate('/vacancie-five')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
+          <button onClick={()=>navigate('/vacancie-seven')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
         </div>
       </div> 
 
@@ -203,7 +203,7 @@ return (
           <p className='font-manrope font-medium text-[13px] uppercase self-center text-right md:col-span-2 md:text-base'>{t.p_vac_name_eight}</p>
           <h4 className='font-bebas text-2xl col-span-full'>{t.h4_price_one_eight}</h4>
           <h5 className='font-manrope text-sm col-span-full md:text-base'>{t.h5_text_one_eight}</h5>
-          <button onClick={()=>navigate('/vacancie-five')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
+          <button onClick={()=>navigate('/vacancie-eight')} className='col-span-full bg-neutal-black text-neutal-white font-bebas text-xl min-h-[45px] rounded-[10px] mt-2.5 md:min-h-[55px] hover:bg-gray-800 focus:bg-gray-800 active:bg-gray-700 transition-colors duration-200'>{t.btn_vac}</button>
         </div>
       </div>
     </section>
